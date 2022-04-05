293076b865b1c63c12f14194298ae25998ae76b9
Michaelstones <michaelshow99@gmail.com>
2022-04-05 16:24:10 +0100

    update

--- back/server.js
+++ back/server.js
@@ -1,5 +1,15 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((err) => console.log(err.message));
 // console.log(data);
 const app = express();
 app.get("/api/products", (req, res) => {
